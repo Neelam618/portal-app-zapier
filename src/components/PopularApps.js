1734155b66ffab379900033c4b1eea1628c46489
Neelam Chavan <neelamchavan410@gmail.com>
2021-09-22 14:58:03 +0530

    layout styles

--- src/components/PopularApps.js
+++ src/components/PopularApps.js
@@ -16,9 +16,9 @@ function PopularApps() {
                     <Content>
                         <Carousel>
                             <div>
-                                <Row gutter={[24, 24]}>
+                                <Row gutter={[0, 0]}>
                                     <Col lg={12}>
-                                        <div className="appContent">
+                                        <a href="" className="appContent">
                                             <div className="appImg">
                                                 <span className="appImgWrapper">
                                                     <img src="https://zapier-images.imgix.net/storage/services/8913a06feb7556d01285c052e4ad59d0.png?auto=format&ixlib=react-9.0.2&ar=undefined&fit=crop&h=40&w=40&q=50&dpr=1" alt="" />
@@ -28,7 +28,7 @@ function PopularApps() {
                                                 <h3>Google Sheets</h3>
                                                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.Esse nulla reprehenderit ipsam repudiandae corporis minus distinctio sint iste saepe ad </p>
                                             </div>
-                                        </div>
+                                        </a>
                                     </Col>
                                     <Col lg={12}>
                                         <div className="appContent">
@@ -46,7 +46,7 @@ function PopularApps() {
                                 </Row>
                             </div>
                             <div>
-                                <Row gutter={[24, 24]}>
+                                <Row gutter={[0, 0]}>
                                     <Col lg={12}>
                                         <div className="appContent">
                                             <div className="appImg">
