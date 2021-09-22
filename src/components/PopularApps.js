255a61f6eea974f43f3d7f242da0fa1132fff817
Neelam Chavan <neelamchavan410@gmail.com>
2021-09-22 18:35:09 +0530

    updated map loop

--- src/components/PopularApps.js
+++ src/components/PopularApps.js
@@ -5,22 +5,60 @@ import PopularAppsApi from './PopularAppsApi';
 
 function PopularApps() {
     const carouselItems = PopularAppsApi().reduce((previousValue, currentValue, currentIndex) => {
-        let row = []; 
-        if(currentIndex % 2 == 1) {
+        let row = [];
+        if (currentIndex % 2 == 1) {
             row = previousValue.splice(-1, 1)[0]
         }
         row.push(currentValue)
         previousValue.push(row)
         return previousValue
-     },[] )
-     console.log(carouselItems);
-        return (
+    }, [])
+    console.log(carouselItems);
+    return (
         <div>
             <Carousel>
-                <div>
+                {
+                    carouselItems.map((items) => {
+                        return (
+                        <div>
+                            <Row gutter={[0, 0]}>
+                                <Col lg={12}>
+                                    <a href="" className="appContent">
+                                        <div className="appImg">
+                                            <span className="appImgWrapper">
+                                                <img src="https://zapier-images.imgix.net/storage/services/8913a06feb7556d01285c052e4ad59d0.png?auto=format&ixlib=react-9.0.2&ar=undefined&fit=crop&h=40&w=40&q=50&dpr=1" alt="" />
+                                            </span>
+                                        </div>
+                                        <div className="appText">
+                                            <h3>{items[0].title}</h3>
+                                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.Esse nulla reprehenderit ipsam repudiandae corporis minus distinctio sint iste saepe ad </p>
+                                        </div>
+                                    </a>
+                                </Col>
+                                {items.length > 1 ? 
+                                <Col lg={12}>
+                                    <div className="appContent">
+                                        <div className="appImg">
+                                            <span className="appImgWrapper">
+                                                <img src="https://zapier-images.imgix.net/storage/services/8913a06feb7556d01285c052e4ad59d0.png?auto=format&ixlib=react-9.0.2&ar=undefined&fit=crop&h=40&w=40&q=50&dpr=1" alt="" />
+                                            </span>
+                                        </div>
+                                        <div className="appText">
+                                            <h3>{items[1].title}</h3>
+                                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.Esse nulla reprehenderit ipsam repudiandae corporis minus distinctio sint iste saepe ad </p>
+                                        </div>
+                                    </div>
+                                </Col>
+                                : null }
+                            </Row>
+                        </div>
+                        )
+                    })
+                }
+                {/* <div>
                     <Row gutter={[0, 0]}>
                         <Col lg={12}>
-                            <a href="" className="appContent">
+                            <div className="appContent">
                                 <div className="appImg">
                                     <span className="appImgWrapper">
                                         <img src="https://zapier-images.imgix.net/storage/services/8913a06feb7556d01285c052e4ad59d0.png?auto=format&ixlib=react-9.0.2&ar=undefined&fit=crop&h=40&w=40&q=50&dpr=1" alt="" />
@@ -30,7 +68,7 @@ function PopularApps() {
                                     <h3>Google Sheets</h3>
                                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.Esse nulla reprehenderit ipsam repudiandae corporis minus distinctio sint iste saepe ad </p>
                                 </div>
-                            </a>
+                            </div>
                         </Col>
                         <Col lg={12}>
                             <div className="appContent">
@@ -46,37 +84,7 @@ function PopularApps() {
                             </div>
                         </Col>
                     </Row>
-                </div>
-                <div>
-                    <Row gutter={[0, 0]}>
-                        <Col lg={12}>
-                            <div className="appContent">
-                                <div className="appImg">
-                                    <span className="appImgWrapper">
-                                        <img src="https://zapier-images.imgix.net/storage/services/8913a06feb7556d01285c052e4ad59d0.png?auto=format&ixlib=react-9.0.2&ar=undefined&fit=crop&h=40&w=40&q=50&dpr=1" alt="" />
-                                    </span>
-                                </div>
-                                <div className="appText">
-                                    <h3>Google Sheets</h3>
-                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.Esse nulla reprehenderit ipsam repudiandae corporis minus distinctio sint iste saepe ad </p>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col lg={12}>
-                            <div className="appContent">
-                                <div className="appImg">
-                                    <span className="appImgWrapper">
-                                        <img src="https://zapier-images.imgix.net/storage/services/8913a06feb7556d01285c052e4ad59d0.png?auto=format&ixlib=react-9.0.2&ar=undefined&fit=crop&h=40&w=40&q=50&dpr=1" alt="" />
-                                    </span>
-                                </div>
-                                <div className="appText">
-                                    <h3>Google Sheets</h3>
-                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.Esse nulla reprehenderit ipsam repudiandae corporis minus distinctio sint iste saepe ad </p>
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-                </div>
+                </div> */}
             </Carousel>
         </div>
     )
--- src/components/PopularAppsApi.js
+++ src/components/PopularAppsApi.js
@@ -12,14 +12,14 @@ function PopularAppsApi() {
         {
           id: 2,
           image: "https://zapier-images.imgix.net/storage/services/8913a06feb7556d01285c052e4ad59d0.png?auto=format&ixlib=react-9.0.2&ar=undefined&fit=crop&h=40&w=40&q=50&dpr=1",
-          title: "allu pakoida",
+          title: "Gmail",
           description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
         },
         {
           id: 3,
           image: "https://zapier-images.imgix.net/storage/services/8913a06feb7556d01285c052e4ad59d0.png?auto=format&ixlib=react-9.0.2&ar=undefined&fit=crop&h=40&w=40&q=50&dpr=1",
-          title: "corn",
+          title: "Slack",
           category: "breakfast",
           price: "10₹",
           description:
@@ -28,7 +28,7 @@ function PopularAppsApi() {
         {
           id: 4,
           image: "https://zapier-images.imgix.net/storage/services/8913a06feb7556d01285c052e4ad59d0.png?auto=format&ixlib=react-9.0.2&ar=undefined&fit=crop&h=40&w=40&q=50&dpr=1",
-          title: "chola",
+          title: "Google Calender",
           category: "lunch",
           price: "50₹",
           description:
