52df49ee3ae3bcc9e4d386a3df10f86b0e952b35
Neelam Chavan <neelamchavan410@gmail.com>
2021-09-28 16:34:07 +0530

    dashboard and its heading added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import './App.css';
 import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
+
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      {/* <Layout /> */}
+      <Dashboard />
     </div>
   );
 }
