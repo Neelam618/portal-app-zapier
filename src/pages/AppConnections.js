deccd9123ea260e11682ed4b8051e7ddfed56ca4
Neelam Chavan <neelamchavan410@gmail.com>
2021-09-30 17:24:55 +0530

    list styles

--- src/pages/AppConnections.js
+++ src/pages/AppConnections.js
@@ -12,6 +12,29 @@ function AppConnections() {
                     <div className="addBtnWrapper"><button className="addBtn">Add connection</button></div>
                 </div>
             </div>
+            <div>
+            <div className="listItem">
+                <div className="listTitleWrapper">
+                    <span className="imgWrapper">
+                        <img src="https://cdn.zapier.com/storage/services/54f0bd6f9c31b757ab20d4c7058dc7c0.32x32.png?auto=format&ar=undefined&fit=crop&ixlib=react-9.0.2&h=28&w=28&q=75&dpr=1" alt="" />
+                    </span>
+                    <span className="listTitle">Gmail</span>
+                </div>
+                <div>
+                    <div className="listDetails">
+                        <div className="connections">
+                            <span className="count">1</span><div>Connection</div>
+                        </div>
+                        <div className="zaps">
+                            <span className="count">2</span><div>Zaps</div>
+                        </div>
+                        <div>
+                            <svg className="arrowIcon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.58984 16.59L13.1698 12L8.58984 7.41L9.99984 6L15.9998 12L9.99984 18L8.58984 16.59Z" fill="#666666"></path></svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
