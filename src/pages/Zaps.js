3c33e404912e08f8d7b218eca0664ff6f401edb4
Neelam Chavan <neelamchavan410@gmail.com>
2021-10-05 18:18:26 +0530

    workflow section styling

--- src/pages/Zaps.js
+++ src/pages/Zaps.js
@@ -30,31 +30,31 @@ function zaps() {
             <div>
                 <Row>
                     <Col lg={8} className="zaps">
-                        <h1 className="heading">Zaps</h1>
-                        <Input size="medium" placeholder="Search by App" prefix={<SearchOutlined />} />
+                        <h1 className="heading">Workflows</h1>
+                        <Input size="medium" placeholder="Filter workflows" prefix={<SearchOutlined />} />
                         <div className="privateFolder">
                             <div className="privateFoldersHeading">
                                 <div><h4 className="title">Private Folders</h4></div>
                                 <div>
-                                    <span>
-                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="#666666"></path></svg>
-                                    </span>
+                                    <a href="">
+                                        <svg className="plusSvg" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="#666666"></path></svg>
+                                    </a>
                                 </div>
                             </div>
                             <div className="privateFolderListItem">
-                                <div>Home</div>
-                                <div>9</div>
+                                <div><a href="" className="folderLink">Home</a></div>
+                                <div className="folderCount">9</div>
                             </div>
                         </div>
                         <div className="sharedFolder">
                             <h4 className="title">Shared Folders</h4>
                             <img src="https://cdn.zapier.com/zapier/images/team-accounts/illustrations/v2/scene-high-five.svg" alt="" />
-                            <div>Need to share your Zaps?</div>
-                            <p>Start collaborating with your team faster with Zapier for Teams. Start your 14-day trial</p>
+                            <div className="comingSoonTitle">Need to share your Workflows?</div>
+                            <p className="comingSoonContent">Start collaborating with your team faster with Zapier for Teams. <a href="" className="trialLink">Start your 14-day trial</a></p>
                         </div>
                         <div className="trash">
-                            <div><a href="">Trash</a></div>
-                            <div>0</div>
+                            <div><a href="" className="trashLink">Trash</a></div>
+                            <div className="trashCount">0</div>
                         </div>
                     </Col>
                     <Col lg={16} className="home">
