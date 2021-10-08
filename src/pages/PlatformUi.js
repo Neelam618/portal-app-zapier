9e391c3df8b315e058fb16d23de3768b85a6d966
Neelam Chavan <neelamchavan410@gmail.com>
2021-10-08 11:55:35 +0530

    sidebar header styling

--- src/pages/PlatformUi.js
+++ src/pages/PlatformUi.js
@@ -25,7 +25,12 @@ function PlatformUi() {
             <Layout>
                 <Content>
                     <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
-                        <Sider className="site-layout-background" width={340}>
+                        <Sider className="site-layout-background" width={340} style={{
+                            overflow: 'auto',
+                            height: '100vh',
+                            position: 'fixed',
+                            left: 0,
+                        }}>
                             <div className="sidebarHeader">
                                 <div className="appTitle">
                                     <div className="appLogo">
@@ -61,26 +66,22 @@ function PlatformUi() {
                             <Menu
                                 mode="inline"
                                 defaultSelectedKeys={['1']}
-                                defaultOpenKeys={['sub1']}
-                                style={{ height: '100%' }}
+                                defaultOpenKeys={['sub1','sub2']}
+                                style={{ height: 650 }}
                             >
-                                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                                    <Menu.Item key="1">option1</Menu.Item>
-                                    <Menu.Item key="2">option2</Menu.Item>
-                                    <Menu.Item key="3">option3</Menu.Item>
-                                    <Menu.Item key="4">option4</Menu.Item>
+                                <SubMenu key="sub1" title="Build">
+                                    <Menu.Item key="1"><a href="">Integration Home</a></Menu.Item>
+                                    <Menu.Item key="2"><a href="">Authentication</a></Menu.Item>
+                                    <Menu.Item key="3"><a href="">Triggers</a></Menu.Item>
+                                    <Menu.Item key="4"><a href="">Actions</a></Menu.Item>
                                 </SubMenu>
-                                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                                    <Menu.Item key="5">option5</Menu.Item>
-                                    <Menu.Item key="6">option6</Menu.Item>
-                                    <Menu.Item key="7">option7</Menu.Item>
-                                    <Menu.Item key="8">option8</Menu.Item>
-                                </SubMenu>
-                                <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                                    <Menu.Item key="9">option9</Menu.Item>
-                                    <Menu.Item key="10">option10</Menu.Item>
-                                    <Menu.Item key="11">option11</Menu.Item>
-                                    <Menu.Item key="12">option12</Menu.Item>
+                                <SubMenu key="sub2" title="Manage">
+                                    <Menu.Item key="5"><a href="">Publishing</a></Menu.Item>
+                                    <Menu.Item key="6"><a href="">Versions</a></Menu.Item>
+                                    <Menu.Item key="7"><a href="">Sharing</a></Menu.Item>
+                                    <Menu.Item key="8"><a href="">Analytics</a></Menu.Item>
+                                    <Menu.Item key="9"><a href="">Monitoring</a></Menu.Item>
+                                    <Menu.Item key="10"><a href="">History</a></Menu.Item>
                                 </SubMenu>
                             </Menu>
                         </Sider>
