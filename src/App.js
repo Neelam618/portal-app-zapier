6ed9c8384cc2c2d31b28f2af4a8abbf90442298e
Neelam Chavan <neelamchavan410@gmail.com>
2021-10-18 13:11:47 +0530

    workflow editor heading content

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Apps from './pages/Apps';
 import Dashboard from './pages/Dashboard';
 import AppConnections from './pages/AppConnections';
 import Workflows from './pages/Workflows';
+import WorkflowEditor from './pages/WorkflowEditor'
 import './style.css'
 import PlatformUi from './pages/platformUI/PlatformUi';
 
@@ -11,9 +12,10 @@ import PlatformUi from './pages/platformUI/PlatformUi';
 function App() {
   return (
     <div className="App">
-      <PlatformUi />
+      {/* <PlatformUi /> */}
       {/* <AppConnections /> */}
       {/* <Workflows /> */}
+      <WorkflowEditor />
       {/* <Apps /> */}
       {/* <Dashboard /> */}
     </div>
