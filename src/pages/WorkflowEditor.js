d77524a2a713dcf77068ca85c1781357e7fc8888
Neelam Chavan <neelamchavan410@gmail.com>
2021-10-20 12:51:07 +0530

    choose app container content

--- src/pages/WorkflowEditor.js
+++ src/pages/WorkflowEditor.js
@@ -26,8 +26,8 @@ function WorkflowEditor() {
    function showAccordion() {
       setIsVisible(true)
    }
-   function showListContent() {
-
+   function showSubListContent() {
+      setIsCollapsed(!isCollapsed)
    }
 
    return (
@@ -88,10 +88,10 @@ function WorkflowEditor() {
                {
                   isVisible ?
                      <div className="subListItemExtended">
-                        <div className="subListItem">
-                           <div className="subListItemTitle" onClick={showListContent}>
+                        <div className="subListItem" onClick={showSubListContent}>
+                           <div className="subListItemTitle">
                               <span className="arrow">
-                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 8.59009L12 13.1701L16.59 8.59009L18 10.0001L12 16.0001L6 10.0001L7.41 8.59009Z" fill="#666666"></path></svg>                           
+                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 8.59009L12 13.1701L16.59 8.59009L18 10.0001L12 16.0001L6 10.0001L7.41 8.59009Z" fill="#666666"></path></svg>
                               </span>
                               Choose app & event
                            </div>
@@ -99,10 +99,25 @@ function WorkflowEditor() {
                               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM5 12L10 17L19 8L17.59 6.58L10 14.17L6.41 10.59L5 12Z" fill="#666666"></path></svg>
                            </div>
                         </div>
-                        <div className="subListItem">
-                           <div className="subListItemTitle" onClick={showListContent}>
+                        {
+                           isCollapsed ?
+                              <div className="subListItemContent">
+                                <div className="chooseAppContainer">
+                                   <div className="selectedApp">
+                                      <img className="selectedAppIcon" src="https://zapier-images.imgix.net/storage/services/6cf3f5a461feadfba7abc93c4c395b33_2.128x128.png?auto=format%2Ccompress&ixlib=python-3.0.0&q=50" alt="" />
+                                       <span className="selectedAppName">App name</span>
+                                   </div>
+                                   <div className="changeText">Change</div>
+                                </div>
+                              </div> : null
+                        }
+
+
+
+                        <div className="subListItem" onClick={showSubListContent}>
+                           <div className="subListItemTitle">
                               <span className="arrow">
-                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 8.59009L12 13.1701L16.59 8.59009L18 10.0001L12 16.0001L6 10.0001L7.41 8.59009Z" fill="#666666"></path></svg>                           
+                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 8.59009L12 13.1701L16.59 8.59009L18 10.0001L12 16.0001L6 10.0001L7.41 8.59009Z" fill="#666666"></path></svg>
                               </span>
                               Choose app & event
                            </div>
