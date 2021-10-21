f8433d3dfa223d0e1d9def0970a29af9f0195e74
Neelam Chavan <neelamchavan410@gmail.com>
2021-10-21 16:58:39 +0530

    updated select

--- src/pages/WorkflowEditor.js
+++ src/pages/WorkflowEditor.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Switch } from 'antd';
-import { Modal } from 'antd';
+import { Modal, Select } from 'antd';
 
 function WorkflowEditor() {
    const [toggleInputField, setToggleInputField] = useState(false)
@@ -8,7 +8,9 @@ function WorkflowEditor() {
    const [inputVal, setInputVal] = useState(data)
    const [isVisible, setIsVisible] = useState(false)
    const [isCollapsed, setIsCollapsed] = useState(false)
+   const [selectInputVal, setSelectInputValue] =useState("")
    const [isModalVisible, setIsModalVisible] = useState(false);
+   const [option,setOption] = useState()
 
    function showInputField() {
       setToggleInputField(true)
@@ -43,6 +45,15 @@ function WorkflowEditor() {
    const handleCancel = () => {
       setIsModalVisible(false);
    };
+
+   // const handleChange = (e) => {
+   //    setSelectInputValue(e.target.value)
+   // }
+
+   function selectHandleChange(value){
+      setOption(value)
+   }
+   const { Option } = Select;
 
    return (
       <div id="editor">
@@ -143,20 +154,32 @@ function WorkflowEditor() {
                                        </div>
                                     </div>
                                  </div>
-                                    
                                  </Modal>
                                  <div className="chooseEventContainer">
                                     <label className="eventLabel">Trigger Event
                                        <span className="requiredText">required</span>
                                     </label>
-                                    <div className="chooseEventInputWrapper">
-                                       <input className="chooseEventInput" type="text" value="Selected event" />
+                                    {/* <div className="chooseEventInputWrapper">
+                                       <input className="chooseEventInput" type="text" value={selectInputVal} onChange={handleChange} />
                                        <div className="chooseEventArrows">
                                           <span>
                                              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.199 19.6172C11.5989 20.1522 12.4009 20.1522 12.8008 19.6172L15.8048 15.5987C16.2977 14.9393 15.8272 14 15.0038 14H8.99595C8.17264 14 7.70206 14.9393 8.19501 15.5987L11.199 19.6172Z" fill="#666666"></path><path d="M11.199 4.40126C11.5989 3.86625 12.4009 3.86625 12.8008 4.40126L15.8048 8.41974C16.2977 9.07917 15.8272 10.0185 15.0038 10.0185H8.99595C8.17264 10.0185 7.70206 9.07917 8.19501 8.41974L11.199 4.40126Z" fill="#666666"></path></svg>
                                           </span>
                                        </div>
-                                    </div>
+                                    </div> */}
+                                       <div className="selectDropdownContainer">
+                                          <Select className="selectListItem" defaultValue="1" style={{ width: "100%" }} onChange={selectHandleChange}>
+                                             <Option value="1">
+                                                <div className="selectListItemTitle">New Public Message Posted Anywhere</div>
+                                                <p className="selectListItemPara">Triggers when a new message is posted to any public channel</p>
+                                             </Option>
+                                             <Option value="2">
+                                                <div className="selectListItemTitle">New Public Message Posted Anywhere 2</div>
+                                                <p className="selectListItemPara">Triggers when a new message is posted to any public channel</p>
+                                             </Option>
+                                          </Select>
+                                       </div>
+                                    
                                     <p className="eventInputInfo">This is what starts the workflow.</p>
 
                                  </div>
