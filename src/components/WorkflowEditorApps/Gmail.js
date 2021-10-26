1b989b9b4f1b622010a65dcc6fc64cac4e5803d8
Neelam Chavan <neelamchavan410@gmail.com>
2021-10-26 14:32:43 +0530

    choose account container

--- src/components/WorkflowEditorApps/Gmail.js
+++ src/components/WorkflowEditorApps/Gmail.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Modal, Select } from 'antd';
 
 function Gmail() {
@@ -7,8 +7,7 @@ function Gmail() {
     const [isChooseAppEventCollapsed, setIsChooseAppEventCollapsed] = useState(false)
     const [isChooseAccountCollapsed, setIsChooseAccountCollapsed] = useState(false)
     const [isModalVisible, setIsModalVisible] = useState(false);
-    const [option, setOption] = useState() 
-
+    const [option, setOption] = useState()
 
     function showAccordion() {
         setIsVisible(true)
@@ -16,31 +15,30 @@ function Gmail() {
 
     function showChooseAppEventContent() {
         setIsChooseAppEventCollapsed(!isChooseAppEventCollapsed)
-     }
-     function showChooseAccountContent() {
+    }
+    function showChooseAccountContent() {
         setIsChooseAccountCollapsed(!isChooseAccountCollapsed)
-     }
-  
-     const showModal = () => {
-        setIsModalVisible(true);
-     };
-  
-     const handleOk = () => {
-        setIsModalVisible(false);
-     };
-  
-     const handleCancel = () => {
-        setIsModalVisible(false);
-     };
-     function selectHandleChange(value) {
-        setOption(value)
-     }
-     
-     const { Option } = Select;
+    }
 
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+    function selectHandleChange(value) {
+        setOption(value)
+    }
+
+    const { Option } = Select;
 
     return (
-        <div className="listItem" onClick={showAccordion}>
+        <div className="listItem" onClick={showAccordion} id="gmail">
             <div className="listItemheader">
                 <div className="listContent">
                     <div className="iconSet">
@@ -48,7 +46,7 @@ function Gmail() {
                             <img className="appIconImg" src="https://zapier-images.imgix.net/storage/services/54f0bd6f9c31b757ab20d4c7058dc7c0.128x128.png?auto=format%2Ccompress&ixlib=python-3.0.0&q=50" alt="" />
                         </div>
                         <div className="statusIcon">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM11 17V15H13V17H11ZM11 7V13H13V7H11Z" fill="#666666"></path></svg>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM11 17V15H13V17H11ZM11 7V13H13V7H11Z" fill="#666666"></path></svg>
                         </div>
                     </div>
                     <div className="listHeading">
@@ -75,7 +73,7 @@ function Gmail() {
                                 Choose app & event
                             </div>
                             <div>
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM5 12L10 17L19 8L17.59 6.58L10 14.17L6.41 10.59L5 12Z" fill="#666666"></path></svg>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM5 12L10 17L19 8L17.59 6.58L10 14.17L6.41 10.59L5 12Z" fill="#666666"></path></svg>
                             </div>
                         </div>
                         {
@@ -94,7 +92,7 @@ function Gmail() {
                                                 <div className="modalContentHeading">
                                                     <div>
                                                         <span className="modalHeadingIcon">
-                                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 8H8V4H4V8ZM10 20H14V16H10V20ZM8 20H4V16H8V20ZM4 14H8V10H4V14ZM14 14H10V10H14V14ZM16 4V8H20V4H16ZM14 8H10V4H14V8ZM16 14H20V10H16V14ZM20 20H16V16H20V20Z" fill="#666666"></path></svg>
+                                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M4 8H8V4H4V8ZM10 20H14V16H10V20ZM8 20H4V16H8V20ZM4 14H8V10H4V14ZM14 14H10V10H14V14ZM16 4V8H20V4H16ZM14 8H10V4H14V8ZM16 14H20V10H16V14ZM20 20H16V16H20V20Z" fill="#666666"></path></svg>
                                                         </span>
                                                     </div>
                                                     <div>
@@ -125,9 +123,7 @@ function Gmail() {
                                                 </Option>
                                             </Select>
                                         </div>
-
                                         <p className="eventInputInfo">This is what starts the workflow.</p>
-
                                     </div>
                                 </div> : null
                         }
@@ -139,14 +135,29 @@ function Gmail() {
                                 Choose account
                             </div>
                             <div>
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM5 12L10 17L19 8L17.59 6.58L10 14.17L6.41 10.59L5 12Z" fill="#666666"></path></svg>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM5 12L10 17L19 8L17.59 6.58L10 14.17L6.41 10.59L5 12Z" fill="#666666"></path></svg>
                             </div>
                         </div>
                         {
                             isChooseAccountCollapsed ?
                                 <div className="subListItemContent">
-                                    <label htmlFor="">Gmail account</label>
-                                    <input type="text" />
+                                    <label htmlFor="">Gmail account: <span>(required)</span></label>
+                                    <div style={{position: 'relative'}}>
+                                        <div className="chooseAccountContainer">
+                                            <div className="chooseAccountIcon">
+                                                <img width="20px" src="https://zapier-images.imgix.net/storage/services/54f0bd6f9c31b757ab20d4c7058dc7c0.128x128.png?auto=format%2Ccompress&ixlib=python-3.0.0&q=50" alt="" />                                            
+                                            </div>
+                                            <button className="chooseAccountBtn">
+                                                <span className="chooseAccountText">Choose an account...</span>
+                                            </button>
+                                            <div className="chooseAccountBtnArrows">
+                                                <span className="chooseAccountBtnArrowsIconWrapper">
+                                                    <svg  style={{height: 'inherit', width: 'inherit', display: 'block'}} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="#424242" fillRule="evenodd" clipRule="evenodd" d="M12.7877 5.00841C12.3874 4.4959 11.612 4.4959 11.2116 5.00841L8.26215 8.78402C7.74926 9.44057 8.21706 10.3996 9.0502 10.3996H14.9492C15.7823 10.3996 16.2501 9.44057 15.7372 8.78402L12.7877 5.00841ZM11.2124 18.9909C11.6127 19.5034 12.3881 19.5034 12.7885 18.9909L15.7379 15.2153C16.2508 14.5587 15.783 13.5996 14.9499 13.5996L9.05093 13.5996C8.2178 13.5996 7.74999 14.5587 8.26288 15.2153L11.2124 18.9909Z"></path></svg>                                            
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p className="chooseAccountInfo">Google Sheets is a secure partner with Zapier. Your credentials are encrypted & can be removed at any time.</p>
                                 </div> : null
                         }
                     </div> : null
--- src/pages/WorkflowEditor.js
+++ src/pages/WorkflowEditor.js
@@ -35,13 +35,13 @@ function WorkflowEditor() {
                         <span>
                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.41 16.59L10.83 12L15.41 7.41L14 6L8 12L14 18L15.41 16.59Z" fill="#666666"></path></svg>
                         </span>
-                        <svg height="24" viewBox="0 0 512 512" width="24" class="css-1sbyvf0"><path d="M318 256c0 19-4 36-10 52-16 7-34 10-52 10-19 0-36-3-52-9-7-17-10-34-10-53 0-18 3-36 10-52 16-6 33-10 52-10 18 0 36 4 52 10 6 16 10 34 10 52zm182-41H355l102-102c-8-11-17-22-26-32-10-9-21-18-32-26L297 157V12c-13-2-27-3-41-3s-28 1-41 3v145L113 55c-12 8-22 17-32 26-10 10-19 21-27 32l102 102H12s-3 27-3 41 1 28 3 41h144L54 399c16 23 36 43 59 59l102-102v144c13 2 27 3 41 3s28-1 41-3V356l102 102c11-8 22-17 32-27 9-10 18-20 26-32L355 297h145c2-13 3-27 3-41s-1-28-3-41z" fill="#ff4a00"></path></svg>
+                        <svg height="24" viewBox="0 0 512 512" width="24"><path d="M318 256c0 19-4 36-10 52-16 7-34 10-52 10-19 0-36-3-52-9-7-17-10-34-10-53 0-18 3-36 10-52 16-6 33-10 52-10 18 0 36 4 52 10 6 16 10 34 10 52zm182-41H355l102-102c-8-11-17-22-26-32-10-9-21-18-32-26L297 157V12c-13-2-27-3-41-3s-28 1-41 3v145L113 55c-12 8-22 17-32 26-10 10-19 21-27 32l102 102H12s-3 27-3 41 1 28 3 41h144L54 399c16 23 36 43 59 59l102-102v144c13 2 27 3 41 3s28-1 41-3V356l102 102c11-8 22-17 32-27 9-10 18-20 26-32L355 297h145c2-13 3-27 3-41s-1-28-3-41z" fill="#ff4a00"></path></svg>
                      </a>
                   </div>
                   <div className="nameWorkflowField">
                      <span>
                         <span className="editIconWrapper">
-                           <svg aria-labelledby="edit-icon-1634539957205.949" class="edit-icon-animated css-voy20m" height="20" viewBox="0 0 24 24" width="20"><title id="edit-icon-1634539957205.949"></title><path fill="#999999" d="M20.71 5.63006C21.1 6.02006 21.1 6.65006 20.71 7.04006L18.88 8.87006L15.13 5.12006L16.96 3.29006C17.35 2.90006 17.98 2.90006 18.37 3.29006L20.71 5.63006ZM3 21.0001V17.2501L14.06 6.19006L17.81 9.94006L6.75 21.0001H3Z"></path></svg>
+                           <svg aria-labelledby="edit-icon-1634539957205.949" height="20" viewBox="0 0 24 24" width="20"><title id="edit-icon-1634539957205.949"></title><path fill="#999999" d="M20.71 5.63006C21.1 6.02006 21.1 6.65006 20.71 7.04006L18.88 8.87006L15.13 5.12006L16.96 3.29006C17.35 2.90006 17.98 2.90006 18.37 3.29006L20.71 5.63006ZM3 21.0001V17.2501L14.06 6.19006L17.81 9.94006L6.75 21.0001H3Z"></path></svg>
                         </span>
                         {
                            toggleInputField ? <input className="inputBox" type="text" value={data} onBlur={() => showWorkflowName(data)} onChange={getInputData} autoFocus onFocus={handleFocus} />
@@ -57,7 +57,6 @@ function WorkflowEditor() {
          </div>
          <div className="main">
             <Gmail />
-
             <div className="topLine"></div>
             {
                showActionPopup ?
@@ -74,7 +73,6 @@ function WorkflowEditor() {
             }
             <div className="bottomLine"></div>
             <div className="arrowHead">▼</div>
-
          </div>
       </div>
    )
