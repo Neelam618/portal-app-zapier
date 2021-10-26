74cadea5ee52ec19cb2d605d9bddfb792860c08d
Neelam Chavan <neelamchavan410@gmail.com>
2021-10-26 11:11:42 +0530

    component for gmail app added

--- src/pages/WorkflowEditor.js
+++ src/pages/WorkflowEditor.js
@@ -1,17 +1,12 @@
 import React, { useState } from 'react'
 import { Switch } from 'antd';
-import { Modal, Select } from 'antd';
 import ActionPopup from '../components/ActionPopup'
+import Gmail from '../components/WorkflowEditorApps/Gmail';
 
 function WorkflowEditor() {
    const [toggleInputField, setToggleInputField] = useState(false)
    const [data, setData] = useState('Name your workflow')
    const [inputVal, setInputVal] = useState(data)
-   const [isVisible, setIsVisible] = useState(false)
-   const [isChooseAppEventCollapsed, setIsChooseAppEventCollapsed] = useState(false)
-   const [isChooseAccountCollapsed, setIsChooseAccountCollapsed] = useState(false)
-   const [isModalVisible, setIsModalVisible] = useState(false);
-   const [option, setOption] = useState()
    const [showActionPopup, setShowActionPopup] = useState(false)
 
    function showInputField() {
@@ -29,32 +24,6 @@ function WorkflowEditor() {
 
    const handleFocus = (event) => event.target.select();         //for selecting input value by default
 
-   function showAccordion() {
-      setIsVisible(true)
-   }
-   function showChooseAppEventContent() {
-      setIsChooseAppEventCollapsed(!isChooseAppEventCollapsed)
-   }
-   function showChooseAccountContent() {
-      setIsChooseAccountCollapsed(!isChooseAccountCollapsed)
-   }
-
-   const showModal = () => {
-      setIsModalVisible(true);
-   };
-
-   const handleOk = () => {
-      setIsModalVisible(false);
-   };
-
-   const handleCancel = () => {
-      setIsModalVisible(false);
-   };
-
-   function selectHandleChange(value) {
-      setOption(value)
-   }
-   const { Option } = Select;
 
    return (
       <div id="editor">
@@ -87,129 +56,7 @@ function WorkflowEditor() {
             </div>
          </div>
          <div className="main">
-            <div className="listItem" onClick={showAccordion}>
-               <div className="listItemheader">
-                  <div className="listContent">
-                     <div className="iconSet">
-                        <div className="appIcon">
-                           <img className="appIconImg" src="https://zapier-images.imgix.net/storage/services/54f0bd6f9c31b757ab20d4c7058dc7c0.128x128.png?auto=format%2Ccompress&ixlib=python-3.0.0&q=50" alt="" />
-                        </div>
-                        <div className="statusIcon">
-                           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM11 17V15H13V17H11ZM11 7V13H13V7H11Z" fill="#666666"></path></svg>
-                        </div>
-                     </div>
-                     <div className="listHeading">
-                        <span className="listSubHeading">Trigger</span>
-                        <div className="title">
-                           1
-                           .
-                           Trigger title
-                        </div>
-                     </div>
-                  </div>
-                  <div>
-                     <button className="editBtn">Edit</button>
-                  </div>
-               </div>
-               {
-                  isVisible ?
-                     <div className="subListItemExtended">
-                        <div className="subListItem" onClick={showChooseAppEventContent}>
-                           <div className="subListItemTitle">
-                              <span className="arrow">
-                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 8.59009L12 13.1701L16.59 8.59009L18 10.0001L12 16.0001L6 10.0001L7.41 8.59009Z" fill="#666666"></path></svg>
-                              </span>
-                              Choose app & event
-                           </div>
-                           <div>
-                              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM5 12L10 17L19 8L17.59 6.58L10 14.17L6.41 10.59L5 12Z" fill="#666666"></path></svg>
-                           </div>
-                        </div>
-                        {
-                           isChooseAppEventCollapsed ?
-                              <div className="subListItemContent">
-                                 <div className="chooseAppContainer" onClick={showModal}>
-                                    <div className="selectedApp">
-                                       <img className="selectedAppIcon" src="https://zapier-images.imgix.net/storage/services/6cf3f5a461feadfba7abc93c4c395b33_2.128x128.png?auto=format%2Ccompress&ixlib=python-3.0.0&q=50" alt="" />
-                                       <span className="selectedAppName">App name</span>
-                                    </div>
-                                    <div className="changeText">Change</div>
-                                 </div>
-                                 <Modal title="Change Trigger" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                                    <div className="modalGrid">
-                                       <div className="leftCol">
-                                          <div className="modalContentHeading">
-                                             <div>
-                                                <span className="modalHeadingIcon">
-                                                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 8H8V4H4V8ZM10 20H14V16H10V20ZM8 20H4V16H8V20ZM4 14H8V10H4V14ZM14 14H10V10H14V14ZM16 4V8H20V4H16ZM14 8H10V4H14V8ZM16 14H20V10H16V14ZM20 20H16V16H20V20Z" fill="#666666"></path></svg>
-                                                </span>
-                                             </div>
-                                             <div>
-                                                <div>
-                                                   <span>App Event</span>
-                                                </div>
-                                                <div>
-                                                   <span className="titlePara">Start the Zap when something happens in an app</span>
-                                                </div>
-                                             </div>
-                                          </div>
-                                       </div>
-                                    </div>
-                                 </Modal>
-                                 <div className="chooseEventContainer">
-                                    <label className="eventLabel">Trigger Event
-                                       <span className="requiredText">required</span>
-                                    </label>
-                                    {/* <div className="chooseEventInputWrapper">
-                                       <input className="chooseEventInput" type="text" value={selectInputVal} onChange={handleChange} />
-                                       <div className="chooseEventArrows">
-                                          <span>
-                                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.199 19.6172C11.5989 20.1522 12.4009 20.1522 12.8008 19.6172L15.8048 15.5987C16.2977 14.9393 15.8272 14 15.0038 14H8.99595C8.17264 14 7.70206 14.9393 8.19501 15.5987L11.199 19.6172Z" fill="#666666"></path><path d="M11.199 4.40126C11.5989 3.86625 12.4009 3.86625 12.8008 4.40126L15.8048 8.41974C16.2977 9.07917 15.8272 10.0185 15.0038 10.0185H8.99595C8.17264 10.0185 7.70206 9.07917 8.19501 8.41974L11.199 4.40126Z" fill="#666666"></path></svg>
-                                          </span>
-                                       </div>
-                                    </div> */}
-                                    <div className="selectDropdownContainer">
-                                       <Select className="selectListItem" defaultValue="1" style={{ width: "100%" }} onChange={selectHandleChange}>
-                                          <Option value="1">
-                                             <div className="selectListItemTitle">New Public Message Posted Anywhere</div>
-                                             <p className="selectListItemPara">Triggers when a new message is posted to any public channel</p>
-                                          </Option>
-                                          <Option value="2">
-                                             <div className="selectListItemTitle">New Public Message Posted Anywhere 2</div>
-                                             <p className="selectListItemPara">Triggers when a new message is posted to any public channel</p>
-                                          </Option>
-                                       </Select>
-                                    </div>
-
-                                    <p className="eventInputInfo">This is what starts the workflow.</p>
-
-                                 </div>
-                              </div> : null
-                        }
-
-                        <div className="subListItem" onClick={showChooseAccountContent}>
-                           <div className="subListItemTitle">
-                              <span className="arrow">
-                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 8.59009L12 13.1701L16.59 8.59009L18 10.0001L12 16.0001L6 10.0001L7.41 8.59009Z" fill="#666666"></path></svg>
-                              </span>
-                              Choose account
-                           </div>
-                           <div>
-                              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM5 12L10 17L19 8L17.59 6.58L10 14.17L6.41 10.59L5 12Z" fill="#666666"></path></svg>
-                           </div>
-                        </div>
-                        {
-                           isChooseAccountCollapsed ?
-                              <div className="subListItemContent">
-                                 <label htmlFor="">Gmail account</label>
-                                 <input type="text" />
-                              </div> : null
-                        }
-
-                     </div> : null
-               }
-
-            </div>
+            <Gmail />
 
             <div className="topLine"></div>
             {
