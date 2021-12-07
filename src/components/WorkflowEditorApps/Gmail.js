c9671946148b83ef209f68dc2fb30c8d214e0161
Neelam Chavan <neelamchavan410@gmail.com>
2021-12-07 10:42:26 +0530

    select input updated

--- src/components/WorkflowEditorApps/Gmail.js
+++ src/components/WorkflowEditorApps/Gmail.js
@@ -8,6 +8,8 @@ function Gmail() {
     const [isVisible, setIsVisible] = useState(false)
     const [isChooseAppEventCollapsed, setIsChooseAppEventCollapsed] = useState(false)
     const [isChooseAccountCollapsed, setIsChooseAccountCollapsed] = useState(false)
+    const [isSetupTriggerCollapsed, setIsSetupTriggerCollapsed] = useState(false)
+
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [showDropdown, setshowDropdown] = useState(false)
     const [option, setOption] = useState()
@@ -32,8 +34,8 @@ function Gmail() {
         setIsChooseAccountCollapsed(!isChooseAccountCollapsed)
     }
 
-    function showSetupActionContent() {
-        
+    function showSetupTriggerContent() {
+        setIsSetupTriggerCollapsed(!isSetupTriggerCollapsed)
     }
 
     const showModal = () => {
@@ -171,25 +173,47 @@ function Gmail() {
                                             <div className="chooseAccountIcon">
                                                 <img width="20px" src="https://zapier-images.imgix.net/storage/services/54f0bd6f9c31b757ab20d4c7058dc7c0.128x128.png?auto=format%2Ccompress&ixlib=python-3.0.0&q=50" alt="" />
                                             </div>
-                                            <button className="chooseAccountBtn" onClick={showAccountOptions} ref={ref}>
-                                                <span className="chooseAccountText">Choose an account...</span>
-                                            </button>
-                                            {
-                                                showDropdown ?
-                                                    <div className="chooseAccountDropdown">
-                                                        <div>Choose...</div>
-                                                        <input className="dropdownSearchInput" type="text" />
-                                                        <Radio.Group name="radiogroup" defaultValue={1}>
-                                                            <Radio value={1}>email@gmail.com</Radio>
+                                            <Select defaultValue="lucy" style={{ width: "100%" }}>
+                                                <Option value="chat">Gmail emailId@gmail.com</Option>                                                
+                                            </Select>
+                                        </div>
 
-                                                        </Radio.Group>
-                                                    </div> : null
-                                            }
-                                            <div className="chooseAccountBtnArrows">
-                                                <span className="chooseAccountBtnArrowsIconWrapper">
-                                                    <svg style={{ height: 'inherit', width: 'inherit', display: 'block' }} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="#424242" fillRule="evenodd" clipRule="evenodd" d="M12.7877 5.00841C12.3874 4.4959 11.612 4.4959 11.2116 5.00841L8.26215 8.78402C7.74926 9.44057 8.21706 10.3996 9.0502 10.3996H14.9492C15.7823 10.3996 16.2501 9.44057 15.7372 8.78402L12.7877 5.00841ZM11.2124 18.9909C11.6127 19.5034 12.3881 19.5034 12.7885 18.9909L15.7379 15.2153C16.2508 14.5587 15.783 13.5996 14.9499 13.5996L9.05093 13.5996C8.2178 13.5996 7.74999 14.5587 8.26288 15.2153L11.2124 18.9909Z"></path></svg>
-                                                </span>
+                                    </div>
+                                    <p className="chooseAccountInfo">Gmail is a secure partner with Zapier. Your credentials are encrypted & can be removed at any time.</p>
+                                    <button className="continueBtn">Continue</button>
+                                </div> : null
+                        }
+
+                        <div className="subListItem" onClick={showSetupTriggerContent}>
+                            <div className="subListItemTitle">
+                                <span className="arrow">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 8.59009L12 13.1701L16.59 8.59009L18 10.0001L12 16.0001L6 10.0001L7.41 8.59009Z" fill="#666666"></path></svg>
+                                </span>
+                                Set up trigger
+                            </div>
+                            <div>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM5 12L10 17L19 8L17.59 6.58L10 14.17L6.41 10.59L5 12Z" fill="#666666"></path></svg>
+                            </div>
+                        </div>
+                        {
+                            isSetupTriggerCollapsed ?
+                                <div className="subListItemContent">
+                                    <label htmlFor="">Label/Mailbox</label>
+                                    <div>
+                                        <div className="chooseAccountContainer">
+                                            <div className="chooseAccountIcon">
+                                                <img width="20px" src="https://zapier-images.imgix.net/storage/services/54f0bd6f9c31b757ab20d4c7058dc7c0.128x128.png?auto=format%2Ccompress&ixlib=python-3.0.0&q=50" alt="" />
                                             </div>
+                                            <Select defaultValue="lucy" style={{ width: "100%" }}>
+                                                <Option value="chat">Chat</Option>
+                                                <Option value="sent">Sent</Option>
+                                                <Option value="inbox">Inbox</Option>
+                                                <Option value="imp">Important</Option>
+                                                <Option value="trash">Trash</Option>
+                                                <Option value="draft">Draft</Option>
+                                                <Option value="span">Span</Option>
+                                            </Select>
+                                                                        
                                         </div>
 
                                     </div>
@@ -198,17 +222,6 @@ function Gmail() {
                                 </div> : null
                         }
 
-                        <div className="subListItem" onClick={showSetupActionContent}>
-                            <div className="subListItemTitle">
-                                <span className="arrow">
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 8.59009L12 13.1701L16.59 8.59009L18 10.0001L12 16.0001L6 10.0001L7.41 8.59009Z" fill="#666666"></path></svg>
-                                </span>
-                                Set up action
-                            </div>
-                            <div>
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM5 12L10 17L19 8L17.59 6.58L10 14.17L6.41 10.59L5 12Z" fill="#666666"></path></svg>
-                            </div>
-                        </div>
                     </div> : null
             }
         </div>
