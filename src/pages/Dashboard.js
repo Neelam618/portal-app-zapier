b05c9c115de6967b070d86d751f6537c70b05480
Neelam Chavan <neelamchavan410@gmail.com>
2021-12-09 12:08:00 +0530

    update class names for nav

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -9,30 +9,30 @@ function Dashboard() {
         <div id="dashboard">
             <div style={{ display: 'flex', justifyContent: 'center', padding: '6em 0'  }}>
                
-                <nav class="nav__cont">
+                <nav class="navContainer">
                 <ul class="nav">
-                        <li class="nav__items ">
+                        <li class="navItems">
                             <span className="createWorkflowIcon">
                                 <svg width="26" height="26" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="#ffffff"></path></svg>
                             </span>
                             <a href="" className="createWorkflowNavText">Create Workflow</a>
                         </li>
 
-                        <li class="nav__items ">
+                        <li class="navItems">
                         <span className="navIcon">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 13H3V3H11V13ZM11 21H3V15H11V21ZM13 21H21V11H13V21ZM13 9V3H21V9H13Z" fill="#666666"></path></svg>                            
                         </span>
                             <a href="">Dashboard</a>
                         </li>
 
-                        <li class="nav__items ">
+                        <li class="navItems">
                         <span className="navIcon">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2.02002 12C2.02002 6.49002 6.49002 2.02002 12 2.02002C17.51 2.02002 21.98 6.49002 21.98 12C21.98 17.51 17.51 21.98 12 21.98C6.49002 21.98 2.02002 17.51 2.02002 12ZM11.48 13.74V20L16.35 10.26H13V4.00002L8.00002 13.74H11.48Z" fill="#666666"></path></svg>
                         </span>
                             <a href="">Zaps</a>
                         </li>
 
-                        <li class="nav__items ">
+                        <li class="navItems">
                         <span className="navIcon">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 8H8V4H4V8ZM10 20H14V16H10V20ZM8 20H4V16H8V20ZM4 14H8V10H4V14ZM14 14H10V10H14V14ZM16 4V8H20V4H16ZM14 8H10V4H14V8ZM16 14H20V10H16V14ZM20 20H16V16H20V20Z" fill="#666666"></path></svg>                            
                         </span>
