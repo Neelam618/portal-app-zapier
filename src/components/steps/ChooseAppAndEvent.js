d56c70944cda54ff3a4682329facd5225a80353e
Neelam Chavan <neelamchavan410@gmail.com>
2021-12-10 19:15:39 +0530

    trigger open second step and close first step

--- src/components/steps/ChooseAppAndEvent.js
+++ src/components/steps/ChooseAppAndEvent.js
@@ -31,6 +31,7 @@ function ChooseAppAndEvent(props) {
     const [showDropdown, setshowDropdown] = useState(false)
     const [option, setOption] = useState()
     const [showChooseAppAndEventStep, setShowChooseAppAndEventStep] = useState(false)
+    const [showNextStep, setShowNextStep] = useState(false)
 
     const showModal = () => {
         setIsModalVisible(true);
@@ -53,6 +54,10 @@ function ChooseAppAndEvent(props) {
         setShowChooseAppAndEventStep(true)
     }
 
+    function openNextStepAndCloseCurrentStep() {
+        setShowNextStep(true)
+    }
+
     return (
         <>
             <div id='chooseAppAndEventStepContainer'>
@@ -65,109 +70,126 @@ function ChooseAppAndEvent(props) {
                         <div className="triggerSubTitle">1. New Event in Google Calendar</div>
                     </div>
                 </div>
-                <div className="chooseAppAndEventContent">
-                    <div className='stepContentTitle'>Choose App & event</div>
-                    <div className="chooseAppContainer" onClick={showModal}>
-                        <div className="selectedApp">
-                            <img className="selectedAppIcon" src="https://zapier-images.imgix.net/storage/services/6cf3f5a461feadfba7abc93c4c395b33_2.128x128.png?auto=format%2Ccompress&ixlib=python-3.0.0&q=50" alt="" />
-                            <span className="selectedAppName">App name</span>
-                        </div>
-                        <div className="changeText">Change</div>
-                    </div>
-                    <Modal centered title="Change Trigger" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                        <div className="actionAndTriggerMain">
-                            <div className="actionAndTriggerGrid">
-                                <div className="leftCol">
-                                    <div className="appEventHeading">
-                                        <div>
-                                            <span className="appEventIcon">
-                                                <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 8H8V4H4V8ZM10 20H14V16H10V20ZM8 20H4V16H8V20ZM4 14H8V10H4V14ZM14 14H10V10H14V14ZM16 4V8H20V4H16ZM14 8H10V4H14V8ZM16 14H20V10H16V14ZM20 20H16V16H20V20Z" fill="#ff4a00"></path></svg>
-                                            </span>
+                {
+                    showNextStep ?
+                        <div className="accordionItem">
+                            <div className="accordionTitle">
+                                <span className="accordionArrow">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.41 8.59009L12 13.1701L16.59 8.59009L18 10.0001L12 16.0001L6 10.0001L7.41 8.59009Z" fill="#666666"></path></svg>
+                                </span>
+                                Choose app & event
+                            </div>
+                            <div>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M2 12C2 6.48 6.48 2 12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12ZM5 12L10 17L19 8L17.59 6.58L10 14.17L6.41 10.59L5 12Z" fill="#666666"></path></svg>
+                            </div>
+                        </div> :
+
+                        <div className="accordionContent">
+                            <div className='stepContentTitle'>Choose App & event</div>
+                            <div className="chooseAppContainer" onClick={showModal}>
+                                <div className="selectedApp">
+                                    <img className="selectedAppIcon" src="https://zapier-images.imgix.net/storage/services/6cf3f5a461feadfba7abc93c4c395b33_2.128x128.png?auto=format%2Ccompress&ixlib=python-3.0.0&q=50" alt="" />
+                                    <span className="selectedAppName">App name</span>
+                                </div>
+                                <div className="changeText">Change</div>
+                            </div>
+                            <Modal centered title="Change Trigger" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                                <div className="actionAndTriggerMain">
+                                    <div className="actionAndTriggerGrid">
+                                        <div className="leftCol">
+                                            <div className="appEventHeading">
+                                                <div>
+                                                    <span className="appEventIcon">
+                                                        <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 8H8V4H4V8ZM10 20H14V16H10V20ZM8 20H4V16H8V20ZM4 14H8V10H4V14ZM14 14H10V10H14V14ZM16 4V8H20V4H16ZM14 8H10V4H14V8ZM16 14H20V10H16V14ZM20 20H16V16H20V20Z" fill="#ff4a00"></path></svg>
+                                                    </span>
+                                                </div>
+                                                <div className="appEventTitleText">
+                                                    <div className="triggerSubTitle">App Event</div>
+                                                    <div className="titlePara">Start the workflow when something happens in an app</div>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <Input id="inputBox" size="large" placeholder="Search apps..." prefix={<SearchOutlined />} />
+                                            </div>
+                                            <div className="triggerAppsContainer">
+                                                {
+                                                    apps.map((appItem) => {
+                                                        return (
+                                                            <div className="triggerAppWrapper" onClick={openChooseAppAndEventStep}>
+                                                                <div className="triggerAppIcon">
+                                                                    <img width="21" src={appItem.appIcon} alt="" />
+                                                                </div>
+                                                                <div className="triggerAppName">{appItem.app}</div>
+                                                            </div>
+                                                        )
+                                                    })
+                                                }
+                                            </div>
                                         </div>
-                                        <div className="appEventTitleText">
-                                            <div className="triggerSubTitle">App Event</div>
-                                            <div className="titlePara">Start the workflow when something happens in an app</div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <Input id="inputBox" size="large" placeholder="Search apps..." prefix={<SearchOutlined />} />
-                                    </div>
-                                    <div className="triggerAppsContainer">
-                                        {
-                                            apps.map((appItem) => {
-                                                return (
-                                                    <div className="triggerAppWrapper" onClick={openChooseAppAndEventStep}>
-                                                        <div className="triggerAppIcon">
-                                                            <img width="21" src={appItem.appIcon} alt="" />
-                                                        </div>
-                                                        <div className="triggerAppName">{appItem.app}</div>
+                                        <div className="rightCol">
+                                            <button className="optionsButton">
+                                                <div className="buttonContent">
+                                                    <div className="buttonIcon">
+                                                        <img width="40" src="https://zapier-images.imgix.net/storage/services/9abbda159e8d9cec0e5eaa8ff1fd5205.png?auto=format&fit=crop&h=50&ixlib=react-9.0.2&w=50&ar=undefined&h=40&w=40&q=75&dpr=1" alt="" />
                                                     </div>
-                                                )
-                                            })
-                                        }
+                                                    <div className="buttonText">
+                                                        <div className="optionsTitle">Schedule</div>
+                                                        <div>Start the workflow every day, hour, or a custom interval</div>
+                                                    </div>
+                                                </div>
+                                            </button>
+                                            <button className="optionsButton">
+                                                <div className="buttonContent">
+                                                    <div className="buttonIcon">
+                                                        <img width="40" src="https://zapier-images.imgix.net/storage/services/603433f546bb15670c1df9309b198816.png?auto=format&fit=crop&h=50&ixlib=react-9.0.2&w=50&ar=undefined&h=40&w=40&q=75&dpr=1" alt="" />                                    </div>
+                                                    <div className="buttonText">
+                                                        <div className="optionsTitle">RSS</div>
+                                                        <div>Start the workflow when an RSS feed updates</div>
+                                                    </div>
+                                                </div>
+                                            </button>
+                                            <button className="optionsButton">
+                                                <div className="buttonContent">
+                                                    <div className="buttonIcon">
+                                                        <img width="40" src="https://zapier-images.imgix.net/storage/services/2b3416ebc79b3fb31c014a3a149633c0.png?auto=format&fit=crop&h=50&ixlib=react-9.0.2&w=50&ar=undefined&h=40&w=40&q=75&dpr=1" alt="" />
+                                                    </div>
+                                                    <div className="buttonText">
+                                                        <div className="optionsTitle">Webhook</div>
+                                                        <div>Start the workflow when another app sends a webhook</div>
+                                                    </div>
+                                                </div>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="rightCol">
-                                    <button className="optionsButton">
-                                        <div className="buttonContent">
-                                            <div className="buttonIcon">
-                                                <img width="40" src="https://zapier-images.imgix.net/storage/services/9abbda159e8d9cec0e5eaa8ff1fd5205.png?auto=format&fit=crop&h=50&ixlib=react-9.0.2&w=50&ar=undefined&h=40&w=40&q=75&dpr=1" alt="" />
-                                            </div>
-                                            <div className="buttonText">
-                                                <div className="optionsTitle">Schedule</div>
-                                                <div>Start the workflow every day, hour, or a custom interval</div>
-                                            </div>
-                                        </div>
-                                    </button>
-                                    <button className="optionsButton">
-                                        <div className="buttonContent">
-                                            <div className="buttonIcon">
-                                                <img width="40" src="https://zapier-images.imgix.net/storage/services/603433f546bb15670c1df9309b198816.png?auto=format&fit=crop&h=50&ixlib=react-9.0.2&w=50&ar=undefined&h=40&w=40&q=75&dpr=1" alt="" />                                    </div>
-                                            <div className="buttonText">
-                                                <div className="optionsTitle">RSS</div>
-                                                <div>Start the workflow when an RSS feed updates</div>
-                                            </div>
-                                        </div>
-                                    </button>
-                                    <button className="optionsButton">
-                                        <div className="buttonContent">
-                                            <div className="buttonIcon">
-                                                <img width="40" src="https://zapier-images.imgix.net/storage/services/2b3416ebc79b3fb31c014a3a149633c0.png?auto=format&fit=crop&h=50&ixlib=react-9.0.2&w=50&ar=undefined&h=40&w=40&q=75&dpr=1" alt="" />
-                                            </div>
-                                            <div className="buttonText">
-                                                <div className="optionsTitle">Webhook</div>
-                                                <div>Start the workflow when another app sends a webhook</div>
-                                            </div>
-                                        </div>
-                                    </button>
+
+                            </Modal>
+                            <div className="chooseEventContainer">
+                                <label className="eventLabel">Trigger Event
+                                    <span className="requiredText">required</span>
+                                </label>
+                                <div className="chooseEventDropdownContainer">
+                                    <Select className="selectListItem" defaultValue="1" style={{ width: "100%" }} onChange={selectHandleChange}>
+                                        <Option value="1">
+                                            <div className="selectListItemTitle">New Public Message Posted Anywhere</div>
+                                            <p className="selectListItemPara">Triggers when a new message is posted to any public channel</p>
+                                        </Option>
+                                        <Option value="2">
+                                            <div className="selectListItemTitle">New Public Message Posted Anywhere 2</div>
+                                            <p className="selectListItemPara">Triggers when a new message is posted to any public channel</p>
+                                        </Option>
+                                    </Select>
                                 </div>
+                                <p className="eventInputInfo">This is what starts the workflow.</p>
+                                <button className="continueBtn" onClick={openNextStepAndCloseCurrentStep}>Continue</button>
                             </div>
                         </div>
-
-                    </Modal>
-                    <div className="chooseEventContainer">
-                        <label className="eventLabel">Trigger Event
-                            <span className="requiredText">required</span>
-                        </label>
-                        <div className="chooseEventDropdownContainer">
-                            <Select className="selectListItem" defaultValue="1" style={{ width: "100%" }} onChange={selectHandleChange}>
-                                <Option value="1">
-                                    <div className="selectListItemTitle">New Public Message Posted Anywhere</div>
-                                    <p className="selectListItemPara">Triggers when a new message is posted to any public channel</p>
-                                </Option>
-                                <Option value="2">
-                                    <div className="selectListItemTitle">New Public Message Posted Anywhere 2</div>
-                                    <p className="selectListItemPara">Triggers when a new message is posted to any public channel</p>
-                                </Option>
-                            </Select>
-                        </div>
-                        <p className="eventInputInfo">This is what starts the workflow.</p>
-                        <button className="continueBtn">Continue</button>
-                    </div>
-                </div>
-                <ChooseAccount />
-                <SetupTrigger />
-                <TestTrigger />
+                }
+                {
+                    showNextStep ? <ChooseAccount /> : null
+                }
+                
+               
 
             </div>
         </>
