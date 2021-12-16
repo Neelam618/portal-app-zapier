2a51623b81417add39ade0fcecf899222cae1a69
Neelam Chavan <neelamchavan410@gmail.com>
2021-12-16 14:07:45 +0530

    signin, signup and auth page added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
+import { useState } from "react"
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import './App.css';
-import Apps from './pages/Apps';
-import Dashboard from './pages/Home';
-import AppConnections from './pages/AppConnections';
-import Workflows from './pages/Workflows';
-import CreateWorkflow from './pages/CreateWorkflow'
 import './style.css'
 import Home from './pages/Home'
-
+import Auth from './components/Auth'
 
 function App() {
+  const [user] = useState(false)
   return (
     <div className="App">
-      {/* <AppConnections /> */}
-      {/* <Workflows /> */}
-      {/* <CreateWorkflow /> */}
-      {/* <Apps /> */}
-      <Home />
+      {
+        user ? <Home /> : <Auth />
+      }
     </div>
   );
 }
